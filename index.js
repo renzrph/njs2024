73d10dc5114b23c558680a5068588a70c264f5eb
renzrph <renzestorrph@gmail.com>
2024-05-19 10:05:57 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// import { getPosts } from "./postController.js";
+
+// pag default
+import getPosts, { getPostsLength } from "./postController.js";
+
+console.log(`Posts Length: ${getPostsLength()}`);
+
+// const { generateRandomNumber, celsiusToFahrenheit } = require("./utils");
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius to Fahrenheit: ${celsiusToFahrenheit(0)}`);
---
+++ postController.js
@@ -0,0 +1,13 @@
+const posts = [
+  { id: 1, title: "Post  One" },
+  { id: 2, title: "Post  Two" },
+];
+
+export const getPosts = () => posts;
+
+// export {getPosts};
+
+export const getPostsLength = () => posts.length;
+
+// pag default
+export default getPosts;
